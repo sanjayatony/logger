aba010602240b9e9db448fcf62f2e25e6428e54a
Tony S <sanjayatony@gmail.com>
2021-08-18 09:21:07 +0700

    create post

---
+++ pages/create-post.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import { supabase } from "../api";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+	ssr: false,
+});
+const initialState = { title: "", content: "" };
+
+function CreatePost() {
+	const [post, setPost] = useState(initialState);
+	const { title, content } = post;
+	const router = useRouter();
+
+	function onChange(e) {
+		setPost(() => ({ ...post, [e.target.name]: e.target.value }));
+	}
+
+	async function createNewPost() {
+		if (!title || !content) return;
+		const user = supabase.auth.user();
+		const id = uuid();
+		post.id = id;
+		const { data } = await supabase
+			.from("posts")
+			.insert([
+				{ title, content, user_id: user.id, user_email: user.email },
+			])
+			.single();
+		router.push(`/post/${data.id}`);
+	}
+	return (
+		<div>
+			<h1 className="mt-6 text-3xl font-semibold tracking-wide">
+				Create new post
+			</h1>
+			<input
+				onChange={onChange}
+				name="title"
+				placeholder="Title"
+				value={post.title}
+				className="w-full pb-2 my-4 text-lg font-light text-gray-500 placeholder-gray-500 border-b focus:outline-none y-2"
+			/>
+			<SimpleMDE
+				value={post.content}
+				onChange={(value) => setPost({ ...post, content: value })}
+			/>
+			<button
+				type="button"
+				className="px-8 py-2 mb-4 font-semibold text-white bg-green-600 rounded-lg"
+				onClick={createNewPost}
+			>
+				Create Post
+			</button>
+		</div>
+	);
+}
+export default CreatePost;
