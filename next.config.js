a6606137d4d7e417a38a93198dc23d4ddf006ede
Tony S <sanjayatony@gmail.com>
2021-08-18 11:20:58 +0700

    add gotrue var

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	reactStrictMode: true,
 	env: {
+    GOTRUE_SITE_URL=process.env.GOTRUE_SITE_URL,
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 		NEXT_PUBLIC_SUPABASE_ANON_KEY:
 			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
