3afb20d8d752282bbc0213ce2cbf8447931b5656
Tony S <sanjayatony@gmail.com>
2021-08-18 09:21:42 +0700

    edit post

---
+++ pages/edit-post/[id].js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import { supabase } from "../../api";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+	ssr: false,
+});
+
+function EditPost() {
+	const [post, setPost] = useState(null);
+	const router = useRouter();
+	const { id } = router.query;
+
+	useEffect(() => {
+		fetchPost();
+		async function fetchPost() {
+			if (!id) return;
+			const { data } = await supabase
+				.from("posts")
+				.select()
+				.filter("id", "eq", id)
+				.single();
+			setPost(data);
+		}
+	}, [id]);
+	if (!post) return null;
+
+	function onChange(e) {
+		setPost(() => ({ ...post, [e.target.name]: e.target.value }));
+	}
+	const { title, content } = post;
+
+	async function updatePost() {
+		if (!title || !content) return;
+		await supabase.from("posts").update([{ title, content }]).match({ id });
+		router.push("/my-posts");
+	}
+	return (
+		<div>
+			<h1 className="mt-6 mb-2 text-3xl font-semibold tracking-wide">
+				Edit post
+			</h1>
+			<input
+				onChange={onChange}
+				name="title"
+				placeholder="Title"
+				value={post.title}
+				className="w-full pb-2 my-4 text-lg font-light text-gray-500 placeholder-gray-500 border-b focus:outline-none y-2"
+			/>
+			<SimpleMDE
+				value={post.content}
+				onChange={(value) => setPost({ ...post, content: value })}
+			/>
+			<button
+				className="px-8 py-2 mb-4 font-semibold text-white bg-blue-600 rounded-lg"
+				onClick={updatePost}
+			>
+				Update Post
+			</button>
+		</div>
+	);
+}
+export default EditPost;
