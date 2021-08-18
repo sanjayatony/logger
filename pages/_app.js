d943296cccd014954291c23af26899b27e776af6
Tony S <sanjayatony@gmail.com>
2021-08-18 09:20:58 +0700

    add supabase wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { supabase } from "../api";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const [user, setUser] = useState(null);
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			async () => checkUser()
+		);
+		checkUser();
+		return () => {
+			authListener?.unsubscribe();
+		};
+	}, []);
+	async function checkUser() {
+		const user = supabase.auth.user();
+		setUser(user);
+	}
+	return (
+		<div>
+			<nav className="p-6 border-b border-gray-300">
+				<Link href="/">
+					<span className="mr-6 cursor-pointer">Home</span>
+				</Link>
+				{user && (
+					<Link href="/create-post">
+						<span className="mr-6 cursor-pointer">Create Post</span>
+					</Link>
+				)}
+				<Link href="/profile">
+					<span className="mr-6 cursor-pointer">Profile</span>
+				</Link>
+			</nav>
+			<div className="px-16 py-8">
+				<Component {...pageProps} />
+			</div>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
