ee258030bb78a4e03ae87fb99b2eeb8710fc05ed
Tony S <sanjayatony@gmail.com>
2021-08-18 11:07:39 +0700

    add env var

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		NEXT_PUBLIC_SUPABASE_ANON_KEY:
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	},
+};
