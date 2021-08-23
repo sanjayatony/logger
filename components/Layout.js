3683d19100591cd2a0290054d5a12dc70b121b5a
Tony S <sanjayatony@gmail.com>
2021-08-24 05:39:06 +0700

    use new layout

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+//import Header from "@/components/Header";
+//import Footer from "@/components/Footer";
+
+export default function Layout({ title, children }) {
+	return (
+		<>
+			<Head>
+				<meta charset="utf8" />
+				<title>{title}</title>
+			</Head>
+
+			<main className="max-w-5xl p-12 mx-auto bg-white shadow-md rounded-3xl">
+				{children}
+			</main>
+		</>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { supabase } from "../api";
-
+import { supabase } from "@/lib/api";
+import ReactMarkdown from "react-markdown";
+import Layout from "@/components/Layout";
 export default function Home() {
 	const [posts, setPosts] = useState([]);
 	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		fetchPosts();
+		const mySubscription = supabase
+			.from("posts")
+			.on("*", () => fetchPosts())
+			.subscribe();
+		return () => supabase.removeSubscription(mySubscription);
 	}, []);
 
 	async function fetchPosts() {
@@ -18,20 +24,25 @@ export default function Home() {
 	if (!posts.length) return <p className="text-2xl">No posts.</p>;
 
 	return (
-		<div>
-			<h1 className="mt-6 mb-2 text-3xl font-semibold tracking-wide">
-				Posts
-			</h1>
-			{posts.map((post) => (
-				<Link key={post.id} href={`/post/${post.id}`}>
-					<div className="pb-4 mt-8 border-b border-gray-300 cursor-pointer">
-						<h2 className="text-xl font-semibold">{post.title}</h2>
-						<p className="mt-2 text-gray-500">
-							Author: {post.user_email}
-						</p>
-					</div>
-				</Link>
-			))}
-		</div>
+		<>
+			<Layout title="Home">
+				<h1 className="mb-2 text-4xl font-bold tracking-wide text-center">
+					Logs
+				</h1>
+				{posts.map((post) => (
+					<Link key={post.id} href={`/post/${post.id}`}>
+						<div className="pb-4 mt-8 border-b border-gray-300 cursor-pointer">
+							<h2 className="text-xl font-semibold">
+								{post.title}
+							</h2>
+							<ReactMarkdown>{post.content}</ReactMarkdown>
+							<p className="mt-2 text-gray-500">
+								Author: {post.user_email}
+							</p>
+						</div>
+					</Link>
+				))}
+			</Layout>
+		</>
 	);
 }
