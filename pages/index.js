0f1f4a65cf962b57c445536c4784cd7da1db9525
Tony S <sanjayatony@gmail.com>
2021-08-24 09:39:12 +0700

    fix button

--- pages/index.js
+++ pages/index.js
@@ -40,31 +40,42 @@ export default function Home() {
 	return (
 		<>
 			<Layout title="Home">
-				<header className="px-2 mb-8 border-purple-500">
-					<h1 className="pb-4 text-4xl font-bold tracking-wide text-center border-b border-purple-400">
+				<div className="mb-6 text-right">
+					<Link href="/create-post">
+						<a className="px-5 py-2 text-lg text-purple-500 border border-purple-500 rounded-full hover:text-white hover:bg-purple-500">
+							+ Write
+						</a>
+					</Link>
+				</div>
+				<header className="px-24 mb-8 border-purple-500">
+					<h1 className="pb-4 text-5xl font-bold tracking-wide text-center border-b border-purple-400">
 						Logs
 					</h1>
 				</header>
-				{posts.map((post) => (
-					<Link key={post.id} href={`/post/${post.id}`}>
-						<div className="p-2 mt-4 cursor-pointer hover:bg-blue-50">
-							<div className="flex items-center">
-								<div className="w-5/6">
-									<h2 className="text-xl">{post.title}</h2>
-									<div className="text-gray-500">
-										{excerptPost(post.content)}
+				<div className="px-6">
+					{posts.map((post) => (
+						<Link key={post.id} href={`/post/${post.id}`}>
+							<div className="p-2 mt-4 cursor-pointer hover:bg-blue-50">
+								<div className="flex items-center">
+									<div className="w-5/6">
+										<h2 className="text-xl">
+											{post.title}
+										</h2>
+										<div className="text-gray-500">
+											{excerptPost(post.content)}
+										</div>
+									</div>
+									<div className="w-1/6 text-right text-gray-500">
+										<ReactTimeAgo
+											date={post.inserted_at}
+											timeStyle="twitter"
+										/>
 									</div>
 								</div>
-								<div className="w-1/6 text-right text-gray-500">
-									<ReactTimeAgo
-										date={post.inserted_at}
-										timeStyle="twitter"
-									/>
-								</div>
 							</div>
-						</div>
-					</Link>
-				))}
+						</Link>
+					))}
+				</div>
 			</Layout>
 		</>
 	);
