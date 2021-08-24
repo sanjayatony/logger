8ab6dc7810d2e14e46c30985c0e8e3c1014e8989
Tony S <sanjayatony@gmail.com>
2021-08-24 10:56:38 +0700

    add header to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-//import Header from "@/components/Header";
+import Header from "@/components/Header";
 //import Footer from "@/components/Footer";
 
 export default function Layout({ title, children, type }) {
@@ -10,10 +10,9 @@ export default function Layout({ title, children, type }) {
 				<meta charset="utf8" />
 				<title>{title}</title>
 			</Head>
+			<Header />
 			{type === "single" ? (
-				<>
-					<main className="">{children}</main>
-				</>
+				<main className="">{children}</main>
 			) : (
 				<main className="max-w-5xl p-12 mx-auto bg-white shadow-md rounded-3xl">
 					{children}
